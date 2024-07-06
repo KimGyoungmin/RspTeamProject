96490d0655b51a8292f52c727c1f1eabe139a2b5
jeil <sjl01002@gmail.com>
2024-07-06 18:22:01 +0900

    db에 데이터 추가 기능

--- app.py
+++ app.py
@@ -12,13 +12,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 
 db = SQLAlchemy(app)
 
+stats = {'win':0, 'lose':0, 'draw':0} # 통계 저장용 딕셔너리
+
+# 데이터베이스 생성
 class Score(db.Model):
     matches = db.Column(db.Integer, primary_key=True) # 게임횟수 저장용
     win = db.Column(db.Integer, nullable=False) # 승리 저장용
     lose = db.Column(db.Integer, nullable=False) # 패배 저장용
     draw = db.Column(db.Integer, nullable=False) #무승부 저장용
-    
-    
+
+# 데이터베이스 삭제 함수(전적초기화 용도)
+def delete_all_scores():
+    try:
+        # 모든 Score 레코드 삭제
+        db.session.query(Score).delete()
+        db.session.commit()
+        print("Score 데이터 삭제 완료!")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Score 데이터 삭제 중 오류 발생: {e}")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -40,24 +54,27 @@ def home():
         
     game_result = result()
 
+    # delete_all_scores() # 데이터 베이스 초기화가 필요할때 활성화하고 다시 실행하세요.
+    
+    if game_result == '승리!':
+        stats['win'] += 1
+    elif game_result == '패배!':
+        stats['lose'] += 1
+    elif game_result == '무승부!':
+        stats['draw'] += 1
+
     context = {
         'computer_pick': computer_choice,
         'your_choice': my_choice,
-        'rog': game_result
+        'rog': game_result,
+        'stats': stats
     }
     # DB 입력부분입니다.
-    score = Score.query.first()
-    if score:
-        if result == '승리!':
-            score.win += 1
-        elif result == '패배!':
-            score.lose += 1
-        elif result == '무승부!':
-            score.draw += 1
-        score.matches += 1
-    else:
-        score = Score(matches=1, win=1 if game_result == '승!' else 0, lose=1 if game_result == '패배!' else 0, draw=1 if game_result == '무승부!' else 0)
-        db.session.add(score)
+    print(context['rog'])
+    score = Score(win = stats['win'], lose = stats['lose'], draw = stats['draw'])
+    
+    db.session.add(score)
+    
     db.session.commit()
     #여기까지 입력부분!
     return render_template('index.html', context=context)
